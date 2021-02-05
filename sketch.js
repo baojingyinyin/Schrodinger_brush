e24846be111a03fa2ed3bfa5eb2c5b47748e5792
baojingyinyin <yuyin22@gmail.com>
2021-02-05 15:36:02 -0800

    upload the file

---
+++ sketch.js
@@ -0,0 +1,26 @@
+// flow and response (s3.2)
+
+let brush;
+
+function setup() {
+  createCanvas(600, 400);
+  strokeWeight(4);
+  background('#023056');
+  noStroke();
+  brush = color(255, 0, 100, 50);
+}
+
+
+function mouseDragged() {
+   fill(brush);
+  ellipse(mouseX, mouseY, 15, 15);
+
+}
+function mouseReleased(){
+  if(random(2)>1){
+    brush = color(255, 0, 100, 50);
+  }else{
+    brush = color(0, 255, 230, 50);
+  }
+  
+}
