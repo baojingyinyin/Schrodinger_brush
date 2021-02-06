6396ea2f3c60372decca940d5acfd4fd35d01d5f
baojingyinyin <yuyin22@gmail.com>
2021-02-05 18:15:16 -0800

    add button for eraser

--- sketch.js
+++ sketch.js
@@ -2,25 +2,48 @@
 
 let brush;
 
+let rNumber = ["1", "2"];
+let thisNumber;
+
+let eraser, brushButton;
+let eraserMode = false;
+let keepdraw;
+
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(800, 500);
   strokeWeight(4);
   background(255);
   noStroke();
   brush = color(255, 0, 100, 50);
+
+  eraser = createButton(String.fromCodePoint(0x26AA));
+  eraser.position(59, 19);
+  eraser.mousePressed(drawWhite);
+  
+  brushButton = createButton(String.fromCodePoint(0x1F58C));
+  brushButton.position(19, 19);
+  brushButton.mousePressed(drawColor);
 }
 
 
 function mouseDragged() {
-   fill(brush);
+  fill(brush);
   ellipse(mouseX, mouseY, 15, 15);
-
 }
-function mouseReleased(){
-  if(random(2)>1){
+
+function mouseReleased() {
+  if (eraserMode) {
+    brush = color(255);
+  } else if (random(rNumber) == 1) {
     brush = color(255, 0, 100, 50);
-  }else{
+  } else {
     brush = color(0, 255, 230, 50);
   }
-  
+}
+
+function drawWhite() {
+  eraserMode = true;
+}
+function drawColor(){
+  eraserMode = false;
 }
